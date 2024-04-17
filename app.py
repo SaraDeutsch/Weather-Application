b139c5b378c0b94928d1620a18b5aca96aeaeff4
SaraDeutsch <155898097+SaraDeutsch@users.noreply.github.com>
2024-04-17 19:52:26 +0000

    final draft

--- app.py
+++ app.py
@@ -2,15 +2,17 @@ from flask import Flask, render_template, request
 from weather import main as get_weather
 
 app = Flask(__name__)
-# home page
 @app.route('/', methods=['GET', 'POST'])
 def index():
     data=None
     if request.method == 'POST':
+        # uses form controls to collect the input
         city = request.form['cityName']
         state = request.form['stateName']
         country = request.form['countryName']
+        # calls main grom weather.py using above input
         data = get_weather(city,state,country)
+    # returns that information to the index.html to format it on the webpage. 
     return render_template('index.html', data=data)
 if __name__ == '__main__':
     app.run(debug=True)
--- weather.py
+++ weather.py
@@ -14,30 +14,37 @@ class WeatherData:
     icon: str
     temperature: int
 
+# this function finds the latitude and longitude for the city and state inputed by the user. 
 def get_lat_and_long(city_name, state_code, country_code, API_key):
     web_info = requests.get(f'http://api.openweathermap.org/geo/1.0/direct?q={city_name},{state_code},{country_code}&appid={API_key}').json()
     data_dict = web_info[0]
     lat, long = data_dict.get('lat'), data_dict.get('lon')
     return lat, long
 
+# this function finds the weather given a latitude and longitude
 def get_current_weather(latitude, longitude, api_key):
     weather = requests.get(f'https://api.openweathermap.org/data/2.5/weather?lat={latitude}&lon={longitude}&appid={api_key}&units=imperial').json()
+    # finds the main weather forecast
     main = weather.get('weather')[0].get('main')
+    # finds the description of that forecase 
     description = weather.get('weather')[0].get('description')
     icon = ""
-    with open('icons.csv') as csv_file:
-        icons = csv.Reader(csv_file)
+    icons_csv_path = '/workspaces/Weather-Application/templates/icons.csv'
+    # iterates through the csv that matches the description to find the appropriate icon's image source
+    with open(icons_csv_path) as csv_file:
+        icons = csv.DictReader(csv_file)
         for item in icons:
-            if item[0] == description:
-                icon = item[2]
-                break
+            if item['weather'] == description:
+                icon = item[' image source']
     temperature =int(weather.get('main').get('temp'))
     data = WeatherData(main, description, icon, temperature)
     return data
+
 def main(city, state, country):
     lat, lon = get_lat_and_long(city, state, country, api_key)
     weather_data = get_current_weather(lat, lon, api_key)
     return weather_data
-# if __name__ == "__main__":
-#     lat, lon = get_lat_and_long('Toronto', 'ON', 'Canada', api_key)
-#     # print(get_current_weather(lat, lon, api_key))
+
+if __name__ == "__main__":
+    lat, lon = get_lat_and_long('Toronto', 'ON', 'Canada', api_key)
+    print(get_current_weather(lat, lon, api_key))
